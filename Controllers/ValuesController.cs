686605908a8e8242781f66e9b50012066b5a2536
진욱 나 <skwlsdnr94@naver.com>
2019-01-15 12:14:00 +0900

    수정

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Test;
 
 namespace Test_Web.Controllers
 {
@@ -14,6 +15,8 @@ namespace Test_Web.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            Class1 c1 = new Class1();
+            Console.WriteLine(c1.GetInt());
             return new string[] { "value1", "value2" };
         }
 
