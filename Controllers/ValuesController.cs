09fc644dbf5089897dfaabf05d0751ee5f8279f8
진욱 나 <skwlsdnr94@naver.com>
2019-01-15 16:02:31 +0900

    DB

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Test;
 using MySql.Data.MySqlClient;
+using Test;
 
 namespace Test_Web.Controllers
 {
